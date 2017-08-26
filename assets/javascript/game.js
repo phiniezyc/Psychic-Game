6c4f983ffb68587ecaa413cd0049ddc2cf81cb65
Chance Phiniezy <phiniezyc@gmail.com>
2017-08-26 01:20:55 -0400

    finished update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,8 @@ var losses = 0;
 var guesses = 9;
 var guessesLeft = 9;
 var userGuesses = [];
-var letterToGuess = null;
+/*Don't need this but good to show letterToGuess not assigned value since it was created within the nextLetterToGuess function*/
+//var letterToGuess = null;
 
 /*We take the length of the array (26) and multiply by random number (gives decimal).  This gives a random letter guaranteed to be inside our array. We then use floor to give us a whole number. Otherwise get decimal.  This is all inside the index position of our computerChoices array to give us a random array each time. */
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -16,31 +17,33 @@ var updateGuessesLeft = function() {
     document.getElementById("guessesLeft").innerHTML = "Guesses left: " + guessesLeft;
 };
 
-var updateLetterToGuess = function() {
+var nextLetterToGuess = function() {
     this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
+
+// display user guesses
 var updateGuessesSoFar = function() {
-    // Here we take the guesses the user has tried -- then display it as letters separated by commas.
-    document.getElementById("guessesSoFar").innerHTML = "Your Guesses so far: " + userGuesses.join(", ");
+    document.getElementById("guessesSoFar").innerHTML = "Gueses You've Made: " + userGuesses.join(" ");
 };
-// Function will be called when we reset everything
+/*This resets the game to get a new letter to guess.  Must refresh to reset game w/ scorboard etc. */
 var reset = function() {
     totalGuesses = 9;
     guessesLeft = 9;
     userGuesses = [];
 
-    updateLetterToGuess();
+    nextLetterToGuess();
     updateGuessesLeft();
     updateGuessesSoFar();
 }
 
-updateLetterToGuess();
+nextLetterToGuess();
 updateGuessesLeft();
 
 
-//When key is released it becomes the users guess
+//When key is released it becomes the users guess. Also starts game.
 document.onkeyup = function(event) {
     guessesLeft--;
+    /*Takes user key pressed and sets all user guesses to lower case. onkey event was used*/
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
     userGuesses.push(userGuess);
@@ -53,14 +56,15 @@ document.onkeyup = function(event) {
             document.getElementById("wins").innerHTML = "Wins: " + wins;
             alert("You got it! You're well on your way to becoming a professional pyschic.");
             reset();
+
         }
     }else if(guessesLeft == 0){
-        // Then we will loss and we'll update the html to display the loss
         losses++;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
         alert("Nope, that's not going to do it. Try Again!");
-        // Then we'll call the reset.
         reset();
     }
+    //reveals the letter the computer picked for testing only.
+    console.log(letterToGuess);
 };
 
